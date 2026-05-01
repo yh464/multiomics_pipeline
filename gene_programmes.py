b79ff3a8f7b00304cd8c291cc35b60beaff2d0e1
Yuankai He <yh464@cam.ac.uk>
2026-05-01 14:31:17 +0100

    bug fix

--- gene_programmes.py
+++ gene_programmes.py
@@ -84,7 +84,11 @@ def factor_celltype_stats(scores, adata, cell_type_key, out_tabular):
     cell_type_mean.columns = [f'F{i+1}_mean' for i, _ in enumerate(cell_type_mean.columns)]
     cell_type_sd = pd.concat(cell_type_sd, axis = 0).set_index(['annot','cell_type'])
     cell_type_sd.columns = [f'F{i+1}_sd' for i, _ in enumerate(cell_type_sd.columns)]
-    cell_type_stats = pd.concat([cell_type_mean, cell_type_sd], axis = 1).sort_index(axis = 1, key = lambda x: x.str.extract(r'F(\d+)_')[0].astype(int))
+    # order columns by F1_mean F1_sd F2_mean F2_sd ...
+    column_order = []
+    for i in range(1, len(cell_type_mean.columns) + 1):
+        column_order.extend([f'F{i}_mean', f'F{i}_sd'])
+    cell_type_stats = pd.concat([cell_type_mean, cell_type_sd], axis = 1).loc[:, column_order]
     cell_type_stats.to_csv(out_tabular, sep = '\t', index = True, header = True)
 
 def factor_correlation(loadings, out_tabular, out_fig):
