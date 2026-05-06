dfed14f60315d672b20f49ceb1a05422b9969a03
Yuankai He <yh464@cam.ac.uk>
2026-05-06 04:15:27 +0100

    test

--- gene_programmes.py
+++ gene_programmes.py
@@ -132,7 +132,7 @@ def factor_embedding(scores, adata, out_fig, embedding_key = ['X_umap'], force =
             embedding_args.append((adata, scores[col], emb_key, figname))
         if len(embedding_args) == 0: continue
         with Pool(processes=min(len(embedding_args),16)) as pool:
-            tqdm(pool.imap(_factor_embedding_single, embedding_args), total=len(embedding_args), desc = f'Plotting factors in {emb_key} space')
+            list(tqdm(pool.imap(_factor_embedding_single, embedding_args), total=len(embedding_args), desc = f'Plotting factors in {emb_key} space'))
     log.log(f'Factor embedding plots saved to {os.path.dirname(out_fig)}', calling_file = 'factor_embedding')
 
 def _factor_timereg_single(input_args):
@@ -161,7 +161,7 @@ def factor_time_reg(scores, adata, out_fig, cell_type_key, time_keys = ['pseudot
         timereg_args = [(scores_tmp, time_key, col, cell_type_key, time_xlabel, out_fig, force) for col in score_cols]
         if len(timereg_args) == 0: continue
         with Pool(processes=min(len(timereg_args),16)) as pool:
-            tqdm(pool.imap(_factor_timereg_single, timereg_args), total=len(timereg_args), desc = f'Plotting factor regression with {time_key}')
+            list(tqdm(pool.imap(_factor_timereg_single, timereg_args), total=len(timereg_args), desc = f'Plotting factor regression with {time_key}'))
     log.log(f'Factor temporal regression plots saved to {os.path.dirname(out_fig)}', calling_file = 'run_cnmf')
 
 def run_cnmf(dataset, prefix, outdir, n_components = range(5, 41, 1), density_threshold = 0.1, 
