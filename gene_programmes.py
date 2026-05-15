0f2e54a2ad757dc038942b8b6885e09324399210
Yuankai He <yh464@cam.ac.uk>
2026-05-15 01:45:29 +0100

    bug fix

--- gene_programmes.py
+++ gene_programmes.py
@@ -145,6 +145,9 @@ def factor_time_reg(scores, adata, out_fig, cell_type_key, time_keys = ['pseudot
         time_xlabel = '' if time_key.find('pseudo') > -1 else time_key.replace('_',' ')
         scores_tmp = pd.concat([scores, adata.obs[[cell_type_key, time_key]]], axis = 1).dropna()  
         for col in tqdm(score_cols, desc = f'Plotting factor temporal regression for {time_key}'):
+            if not isinstance(score_cols[col].dtype, np.number):
+                log.warn(f'Time key {time_key} is not numeric, skipping regression plot for {col}.', calling_file = 'factor_time_reg')
+                continue
             fig_1order = out_fig.replace('$factor', col).replace('$timekey', time_key)
             fig_2order = out_fig.replace('$factor', col).replace('$timekey', time_key).replace('.png', '_order2.png')
             if os.path.isfile(fig_1order) and os.path.isfile(fig_2order) and not force: continue
