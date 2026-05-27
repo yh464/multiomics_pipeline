c6ee39bcdda4488c9941729177705579a8a1456f
Yuankai He <yh464@cam.ac.uk>
2026-05-27 17:24:28 +0100

    bug fix

--- gene_programmes.py
+++ gene_programmes.py
@@ -359,6 +359,7 @@ def run_cnmf(dataset, prefix, outdir, n_components = range(5, 41, 1), density_th
                 projection_loadings = pd.read_table(projection_loadings, index_col = 0)
                 projection_loadings = projection_loadings.loc[:, projection_loadings.columns.intersection(adata.var_names)]
             adata = sc.read_h5ad(h5ad_old, 'r')
+            with open(f'{outdir}/{projection_prefix}/{projection_prefix}_cnmf_k{projection_loadings.shape[0]}_enrichr.txt', 'w') as f: print('', file = f)
             cnmf_downstream(projected_usages, projection_loadings, adata, cell_type, outdir, projected_prefix, projection_loadings.shape[0], force, savedir = None)
         
         return # do not register on the progress file if it is a projection
