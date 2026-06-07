d49a4d6d44e470c11355cbafe0ce9a83652ca284
Yuankai He <yh464@cam.ac.uk>
2026-06-07 01:07:44 +0100

    bug fix

--- score_batch.py
+++ score_batch.py
@@ -30,7 +30,7 @@ def main(args):
         h5 = proj.to_pathname('raw', dataset, prefix)
         adata = sc.read_h5ad(h5, 'r')
         cell_type = [col for col in args.cell_type if col in adata.obs.columns]
-        if cell_type is []: cell_type = get_metadata_cols(dataset, prefix, input_string = 'cell type', adata = adata)
+        if len(cell_type) == 0: cell_type = get_metadata_cols(dataset, prefix, input_string = 'cell type', adata = adata)
         # cmd = ['Rscript', 'sc_score.r','-i', h5, '-o', f'{args.out}/{prefix}', '--label'] + args.label
         cmd = ['python', 'sc_score_cepo.py',dataset, prefix,'-o', args.out, '--label'] + cell_type
         if args.cepo: cmd.append('--cepo')
